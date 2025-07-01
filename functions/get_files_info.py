a3bce9a2b95d60499844eaf205870ad85547834a
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-06-30 23:32:53 -0700

    check if dir is subdir, or dir

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,19 @@
+from os.path import abspath, isdir
+
+
 def get_files_info(working_directory, directory=None):
+    #
+
+    dir_str = abspath(directory)
+    workdir_str = abspath(working_directory)
+
+    if not isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+
+    if not dir_str.startswith(workdir_str):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    # get list of files and make a string
+    # file_name, file_size, is_dir
+    # use os.listdir to get files, then build dict of info?
+    # os.path.getsize() | os.path.isfile()
