87cb5d3dd2eb08fb4408efbf314691246748aa55
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-08-07 00:14:41 -0700

    fixed tests

--- tests.py
+++ tests.py
@@ -8,30 +8,36 @@ class TestGetFiles(unittest.TestCase):
     def test_same_dir(self):
         result = """- tests.py: file_size: 1343 bytes, is_dir=False
 - main.py: file_size: 576 bytes, is_dir=False
-- pkg: file_size: 128 bytes, is_dir=True
-"""
+- pkg: file_size: 128 bytes, is_dir=True"""
+        print("Result for current directory:")
         print(get_files_info("calculator", "."))
+        print("")
         self.assertEqual(get_files_info("calculator", "."), result)
 
     def test_inside_work_dir(self):
         result = """- render.py: file_size: 768 bytes, is_dir=False
-- calculator.py: file_size: 1739 bytes, is_dir=False
-"""
+- calculator.py: file_size: 1739 bytes, is_dir=False"""
+        print("Result for 'pkg' directory:")
         print(get_files_info("calculator", "pkg"))
+        print("")
         self.assertEqual(get_files_info("calculator", "pkg"), result)
 
     def test_outside_work_dir(self):
         result = (
             'Error: Cannot list "/bin" as it is outside the permitted working directory'
         )
+        print("Result for '/bin' directory:")
         print(get_files_info("calculator", "/bin"))
+        print("")
         self.assertEqual(get_files_info("calculator", "/bin"), result)
 
     def test_relative_outside_work_dir(self):
         result = (
             'Error: Cannot list "../" as it is outside the permitted working directory'
         )
+        print("Result for '../' directory:")
         print(get_files_info("calculator", "../"))
+        print("")
         self.assertEqual(get_files_info("calculator", "../"), result)
 
 
