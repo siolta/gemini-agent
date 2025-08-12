ba86094fba4001372c844a51b6216377ca4e5761
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-08-11 22:07:55 -0700

    initial file info func

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,5 +2,11 @@ from os.path import abspath, join, isfile
 
 
 def get_file_content(working_directory, file_path):
+    abs_workdir = abspath(working_directory)
+    target_dir = abspath(join(working_directory, file_path))
+
+    if not target_dir.startswith(abs_workdir):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+
     pass
 
