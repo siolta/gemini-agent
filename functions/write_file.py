521922f0399df0e15934b764b3e5425c2f0fc721
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-08-19 17:54:50 -0700

    add function to write files

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,5 +1,29 @@
-from os.path import abspath, join, isfile
+from os.path import abspath, join, exists, dirname, isdir
+from os import makedirs
 
 
 def write_file(working_directory, file_path, content):
-    pass
+    abs_workdir = abspath(working_directory)
+    target_file = abspath(join(working_directory, file_path))
+
+    if not target_file.startswith(abs_workdir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+    create_dir = dirname(abspath(file_path))
+    if not exists(create_dir):
+        try:
+            makedirs(create_dir, exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if exists(create_dir) and isdir(create_dir):
+        return f'Error: "{file_path}" is a directory, not a file'
+
+    try:
+        with open(target_file, "w") as file:
+            file.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+
+    except Exception as e:
+        return f"Error writing to file: {e}"
--- tests.py
+++ tests.py
@@ -2,99 +2,20 @@ import unittest
 
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
-class TestGetFiles(unittest.TestCase):
-    # def tests here
-    def test_same_dir(self):
-        result = """- lorem.txt: file_size: 67855 bytes, is_dir=False
-- tests.py: file_size: 1343 bytes, is_dir=False
-- main.py: file_size: 576 bytes, is_dir=False
-- pkg: file_size: 128 bytes, is_dir=True"""
-        print("Result for current directory:")
-        print(get_files_info("calculator", "."))
-        print("")
-        self.assertEqual(get_files_info("calculator", "."), result)
-
-    def test_inside_work_dir(self):
-        result = """- render.py: file_size: 768 bytes, is_dir=False
-- calculator.py: file_size: 1739 bytes, is_dir=False"""
-        print("Result for 'pkg' directory:")
-        print(get_files_info("calculator", "pkg"))
-        print("")
-        self.assertEqual(get_files_info("calculator", "pkg"), result)
-
-    def test_outside_work_dir(self):
-        result = (
-            'Error: Cannot list "/bin" as it is outside the permitted working directory'
-        )
-        print("Result for '/bin' directory:")
-        print(get_files_info("calculator", "/bin"))
-        print("")
-        self.assertEqual(get_files_info("calculator", "/bin"), result)
-
-    def test_relative_outside_work_dir(self):
-        result = (
-            'Error: Cannot list "../" as it is outside the permitted working directory'
-        )
-        print("Result for '../' directory:")
-        print(get_files_info("calculator", "../"))
-        print("")
-        self.assertEqual(get_files_info("calculator", "../"), result)
-
-
-class TestGetFileContent(unittest.TestCase):
-    def test_file_in_work_dir(self):
-        func = get_file_content("calculator", "main.py")
-        result = "def main()"
-        print("Result for 'main.py' file:")
-        print(func[:100])
-        print("")
-        self.assertIn(result, func)
-
-    def test_file_in_sub_work_dir(self):
-        func = get_file_content("calculator", "pkg/calculator.py")
-        result = "def _apply_operator(self, operators, values)"
-        print("Result for 'pkg/calculator.py' file:")
-        print(func[:100])
-        print("")
-        self.assertIn(result, func)
-
-    def test_file_outside_work_dir(self):
-        # (this should return an error string)
-        func = get_file_content("calculator", "/bin/cat")
-        result = 'Error: Cannot read "/bin/cat" as it is outside the permitted working directory'
-        print("Result for '/bin/cat' file:")
-        print(func)
-        print("")
-        self.assertIn(result, func)
-
-    def test_file_does_not_exist(self):
-        # (this should return an error string)
-        func = get_file_content("calculator", "pkg/does_not_exist.py")
-        result = (
-            'Error: File not found or is not a regular file: "pkg/does_not_exist.py"'
-        )
-        print("Result for 'pkg/does_not_exist.py' file:")
-        print(func)
-        print("")
-        self.assertIn(result, func)
-
-
-class TestWriteFile(unittest.TestCase):
-    def test_write_existing_file(self):
-        pass
-
-
-class TestBDCases(unittest.TestCase):
+class TestOutput(unittest.TestCase):
     def test(self):
-        result = get_file_content("calculator", "main.py")
+        result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
         print(result)
 
-        result = get_file_content("calculator", "pkg/calculator.py")
+        result = write_file(
+            "calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"
+        )
         print(result)
 
-        result = get_file_content("calculator", "/bin/cat")
+        result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
         print(result)
 
 
